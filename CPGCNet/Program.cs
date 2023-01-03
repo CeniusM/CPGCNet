e39cc8f4e0ce1e18956df017736572a1a62e7187
CeniusM <93879193+CeniusM@users.noreply.github.com>
2023-01-03 03:39:19 +0100

    Update Program.cs

--- CPGCNet/Program.cs
+++ CPGCNet/Program.cs
@@ -11,7 +11,7 @@
  * Better const/varible expersions
  * Remove Any spaves from names
  * Add debuging util 
-
+ * Change the node func to use another rather that a ValueContainor
  */
 
 using CPGCNet;
